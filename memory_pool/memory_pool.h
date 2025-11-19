77034fc7802edba512f9a7be4477447f792dd1ca
cdg <875847402@qq.com>
2025-11-19 22:30:41 +0800

    2025.11.19 搭了内存池框架

---
+++ memory_pool/memory_pool.h
@@ -0,0 +1,20 @@
+#ifndef _MEMORY_POOL_
+#define _MEMORY_POOL_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
